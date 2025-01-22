6fcee0f828e8d278144c1ba5b359943cc2a2e774
Jaseem <jaseemkunji2000@gmail.com>
2025-01-22 13:48:28 +0530

    final commit

--- main.py
+++ main.py
@@ -63,20 +63,3 @@ if __name__ == '__main__':
 
 
 
-
-# tasks = []
-
-# @app.route("/")
-# def index():
-#     return render_template("index.html",tasks=tasks)
-
-# @app.route('/add', methods=['POST'])
-# def add_task():
-#     task = request.form.get('task')
-#     if task:
-#         tasks.append(task)
-#     return redirect(url_for('index'))
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
